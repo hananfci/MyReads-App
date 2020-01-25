80900e60632f06c6040c4152a5a690fa6183741c
Hanan Mostafa <36992480+hananfci@users.noreply.github.com>
2020-01-25 12:59:37 +0200

    Create Books.js

---
+++ src/Books.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Books extends Component {
+   /*
+  constructor(props) {
+    super(props);
+    this.booksByuser = {};
+
+   profiles.forEach(profile => {
+      const movieID = profile.favoriteMovieID;
+
+      if (this.usersByMovie[movieID]) {
+        this.usersByMovie[movieID].push(profile.userID);
+      } else {
+        this.usersByMovie[movieID] = [profile.userID];
+      }
+    }); 
+  }*/
+
+
+  render() {
+    return (
+     <p>test</p>
+    );
+  }
+}
+
+export default Books;
